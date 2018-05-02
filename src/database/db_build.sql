03309fa171fb41b557ed8ab2b5b668edd662fd85
amusamhe <abdalsamad.y.m@gmail.com>
2018-05-02 14:46:08 +0300

    create the sql build with fake data

--- src/database/db_build.sql
+++ src/database/db_build.sql
@@ -7,18 +7,18 @@ CREATE TABLE users(
   user_name VARCHAR UNIQUE NOT NULL ,
   password VARCHAR NOT NULL CHECK (length(password)>=8) ,
   role VARCHAR DEFAULT 'user',
-  email VARCHAR(30) UNIQUE
+  email VARCHAR(30) UNIQUE,
+  first_name VARCHAR(50) NOT NULL,
+  last_name VARCHAR(50) NOT NULL,
 );
 
 CREATE TABLE posts (
   id SERIAL PRIMARY KEY,
-  user_id  INTEGER REFERENCES users(id) ON DELETE CASCADE,
-  pic_url   VARCHAR UNIQUE ,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  pic_url VARCHAR UNIQUE ,
   title VARCHAR NOT NULL ,
-  description TEXT NOT NULL ,
-  post_content TEXT NOT NULL CHECK (length(post_content)>0),
-  created_at   timestamp NOT NULL,
-
+  description TEXT NOT NULL CHECK (length(post_content)>0),
+  created_at timestamp NOT NULL,
 );
 
 CREATE TABLE comments (
@@ -28,14 +28,35 @@ CREATE TABLE comments (
   comment_content TEXT NOT NULL
 );
 
-INSERT INTO users (user_name,password,role,email)
-VALUES ()
+CREATE TABLE likes (
+  id SERIAL PRIMARY KEY,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  post_id INTEGER REFERENCES posts(id) ON DELETE CASCADE, 
+)
 
-INSERT INTO posts (user_id,pic_url,title,description,post_content,created_at)
-VALUES ()
+INSERT INTO users (user_name,password,email, first_name, last_name)
+VALUES 
+  ('m123', '123', 'someone@gmail.com','Ali', 'Baba'),
+  ('john159', '123', 'john@gmail.com', 'John', 'Rees'),
+  ('root987', '147', 'root@gmail.com', 'Root', 'Fruit'),
+  ('lion', '852', 'lion@gmail.com', 'Lion', 'Heart');
+
+INSERT INTO posts (user_id,pic_url,title,description,created_at)
+VALUES 
+  (1, './pic1.jpg', 'Nice pic', 'This is cool a cool pic  123','2018-03-22 19:10:25-07' ),
+  (2, './pic2.jpg', 'Nice pic2', 'This is cool a cool pic321','2018-03-22 19:10:25-07' ),
+  (3, './pic3.jpg', 'Nice pic3', 'This is cool a cool pic213','2018-03-22 19:10:25-07' );
 
 INSERT INTO comments (user_id,post_id ,comment_content)
-VALUES ()
+VALUES 
+  (2, 1, 'This is a great picture'),
+  (2, 3, 'This is a great picture'),
+  (1, 2, 'This is a great picture');
 
+INSERT INTO likes (user_id,post_id )
+VALUES 
+  (2, 1),
+  (2, 3),
+  (1, 2);
 
 COMMIT ;
