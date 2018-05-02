2f9bb99ff7125f201a347046ad2a129648fb3a3e
marwa <marwajomaa4@gmail.com>
2018-05-02 17:15:33 +0300

    express

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,6 +1,16 @@
 // require the other pages
 
+const express = require('express');
+const path = require('path');
+const router = express.Router();
+
+const login = require('./login');
+const register = require('./register');
+const  uploadpic = require('./uploadpic');
+const viewpic = require('./viewpic')
+const error = require('./error');
+
 
 
 // export your functions
-module.exports = {}
+module.exports = router;
